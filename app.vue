e9c52058eaab47cc60b593b1f425c0b70394fee1
Julio Cesar <jcmljunior@proton.me>
2025-03-04 19:44:05 -0300

    Configuração inicial do projeto NuxtJS e Bootstrap.

---
+++ app.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="container">
+        <h1>Fazer Login</h1>
+        <h2>Use sua conta do ??</h2>
+        <form class="mb-3 mt-3">
+            <div class="form-floating mb-3">
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                <label for="floatingInput">Endereço de email</label>
+                <div id="emailHelp" class="form-text">Nunca compartilharemos seu e-mail com mais ninguém.</div>
+            </div>
+            <div class="form-floating">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword">Senha</label>
+            </div>
+            <p class="mt-3">Não está no seu computador? Use o modo visitante para fazer login com privacidade. <a
+                    href="#">Saiba como
+                    usar o modo visitante.</a></p>
+
+            <button disabled type="submit" class="btn btn-primary">Entrar</button>
+        </form>
+        <ul>
+            <li>
+                <NuxtLink to="/register">Ainda não tem uma conta?</NuxtLink>
+            </li>
+            <li>
+                <NuxtLink to="/recovery">Esqueceu sua senha?</NuxtLink>
+            </li>
+        </ul>
+    </div>
+
+    <hr>
+
+    <div class="container">
+        <h1>Criar uma conta do ??</h1>
+        <form>
+            <div class="row g-2">
+                <div class="col-md">
+                    <div class="form-floating mb-3">
+                        <input type="text" class="form-control" id="floatingInput" placeholder="John">
+                        <label for="floatingInput">Nome</label>
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="form-floating mb-3">
+                        <input type="text" class="form-control" id="floatingInput" placeholder="Doe">
+                        <label for="floatingInput">Sobrenome</label>
+                    </div>
+                </div>
+            </div>
+
+            <div class="form-floating mb-3">
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                <label for="floatingInput">Endereço de email</label>
+                <div id="emailHelp" class="form-text">Nunca compartilharemos seu e-mail com mais ninguém.</div>
+            </div>
+
+            <div class="form-floating mb-3">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword">Senha</label>
+            </div>
+
+            <div class="form-floating mb-3">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword">Confirmar Senha</label>
+            </div>
+
+            <button disabled type="submit" class="btn btn-primary">Criar Conta</button>
+        </form>
+
+        <ul>
+            <li>
+                <NuxtLink to="/login">Já tem uma conta?</NuxtLink>
+            </li>
+        </ul>
+    </div>
+</template>
